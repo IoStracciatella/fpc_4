67a4c106264d5c64dbab3bc3e178eadeeb596f2d
Stracciatella <105241465+IoStracciatella@users.noreply.github.com>
2024-12-12 01:22:05 -0300

    Embaçando Imagens

--- Processamento de Imagens/exercicio3.py
+++ Processamento de Imagens/exercicio3.py
@@ -1,26 +1,47 @@
-import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import numpy as np
 
-img = mpimg.imread('/home/zegui/FPC/lista04/processamento_imagem/stinkbug.png')
-height, width, tmp = img.shape
+nIteracoes = int(input('Quantas iterações de suavização você quer aplicar? n: '))
 
-def suavizar(matriz, height, width, iteracoes):
-    for n in range(iteracoes):
-        matriz_suavizada = np.copy(matriz)
-        for i in range(1, height - 1):
-            for j in range(1, width - 1):
-                 matriz_suavizada[i][j] = (matriz[i][j] + matriz[i+1][j] + matriz[i-1][j] + matriz[i][j+1] + matriz[i][j-1] + matriz[i-1][j-1] + matriz[i-1][j+1] + matriz[i+1][j-1] + matriz[i+1][j+1])/ 9
-                 matriz = np.copy(matriz_suavizada)
-    return matriz
+# Requer sobrecargas de, uma matriz, e quantas vezes vc quer iterar a suavização
+def suavizar(imagem, iteracoes):
+    
+    # Converte a imagem para escala de cinza, caso ela ainda esteja colorida
+    if len(imagem.shape) == 3: #"Se tiver dimensão de cor"
+        imagem = np.mean(imagem, axis=2) #"Transforme em uma imagem com só 2 dimensões, sem cor"
+    
+    linhas, colunas = imagem.shape
+    
+    # Copia a imagem original p suavizar depois (não daria certo só referenciar a mesma imagem)
+    imgCopia = np.copy(imagem)
+    
+    for _ in range(iteracoes):
 
+        # Novo array (com a imagem) para armazenar uma iteração
+        img_temp = np.copy(imgCopia)
+        
+        for i in range(1, linhas-1):
+            for j in range(1, colunas-1):
+                img_temp[i, j] = (imgCopia[i, j] + imgCopia[i+1, j] + imgCopia[i-1, j] + imgCopia[i, j+1] + imgCopia[i, j-1]) / 5
+        
+        # Atualizando com novos valores
+        imgCopia = img_temp
+    
+    return imgCopia
 
-img_suavizada = suavizar(img, height, width, iteracoes = 10)
+image = mpimg.imread('stinkbug.png')
 
-plt.title("Original")
-plt.imshow(img)
-plt.show()
+img_suavizada = suavizar(image, iteracoes=nIteracoes)
 
-plt.title("Suavizada")
-plt.imshow(img_suavizada)
+plt.figure(figsize=(10, 5))
+
+plt.subplot(1, 2, 1)
+plt.title('Original')
+plt.imshow(image, cmap='gray')
+
+plt.subplot(1, 2, 2)
+plt.title('Suavizada')
+plt.imshow(img_suavizada, cmap='gray')
+
 plt.show()
